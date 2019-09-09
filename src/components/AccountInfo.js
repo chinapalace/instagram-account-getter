59513ab08dd561562d0bde3f5c5d0a31f4f0e8db
Justin McLaren <justin.mclaren@thehubhaus.com>
2019-09-08 21:22:21 -0700

    Remove extra files

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const AccountInfo = (props) => {
   const accounts = props.accounts[0].output
   const accountItems =  Object.keys(accounts).map(function(key) {
-          return <li>Key: {key}, Value: {accounts[key]}</li>})
+    return <li>Key: {key}, Value: {accounts[key]}</li>})
 
   return (
     <div>
